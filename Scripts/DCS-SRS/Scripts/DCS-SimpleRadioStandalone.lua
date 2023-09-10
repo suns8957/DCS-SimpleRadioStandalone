43cbb6bf51c5ff7abdbac097c4516d00a35d52ac
Ciaran Fisher <cfisher88@gmail.com>
2023-09-10 19:14:38 +0200

    Version bump

--- Scripts/DCS-SRS/Scripts/DCS-SimpleRadioStandalone.lua
+++ Scripts/DCS-SRS/Scripts/DCS-SimpleRadioStandalone.lua
@@ -1,4 +1,4 @@
--- Version 2.0.8.6
+-- Version 2.0.8.7
 -- Special thanks to Cap. Zeen, Tarres and Splash for all the help
 -- with getting the radio information :)
 -- Run the installer to correctly install this file
@@ -5406,4 +5406,4 @@ end
 -- Load mods' SRS plugins
 SR.LoadModsPlugins()
 
-SR.log("Loaded SimpleRadio Standalone Export version: 2.0.8.6")
+SR.log("Loaded SimpleRadio Standalone Export version: 2.0.8.7")
