ca2d024d7c5874adf0ae148e47ffddc132ba440a
Omelette 蛋卷 <phildu202@yahoo.com>
2023-12-28 16:19:43 -0500

    cn-2.0.9.1

--- Installer/App.xaml.cs
+++ Installer/App.xaml.cs
@@ -16,7 +16,7 @@ namespace Installer
         public string[] Arguments = new string[0];
         private void ApplicationStartup(object sender, StartupEventArgs e)
         {
-            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("zh-CN");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("zh-CN");
             if (e.Args.Length > 0)
             {
                 Arguments = e.Args;
