ee5fa747b702c46df94c7724b0f9cbd18052b2a1
gcask <53709079+gcask@users.noreply.github.com>
2025-06-06 22:36:51 -0400

    JSon deserialization of Filters.

--- Common/Audio/Dsp/IFilter.cs
+++ Common/Audio/Dsp/IFilter.cs
@@ -1,5 +1,8 @@
-﻿namespace Ciribob.DCS.SimpleRadio.Standalone.Common.Audio.Dsp
+﻿using System.Text.Json.Serialization;
+
+namespace Ciribob.DCS.SimpleRadio.Standalone.Common.Audio.Dsp
 {
+    [JsonConverter(typeof(JsonDspIFilterConverter))]
     internal interface IFilter
     {
         float Transform(float input);
