d7c382f47e34f8a2f44f64c019e844a16386e187
Ciaran Fisher <cfisher88@gmail.com>
2025-08-03 17:41:04 +0200

    Bump to 2.3.0.0
    Bump version to 2.3 as its a pretty big under the hood change

--- Common/Helpers/UpdaterChecker.cs
+++ Common/Helpers/UpdaterChecker.cs
@@ -28,7 +28,7 @@ public class UpdaterChecker
 
     public static readonly string MINIMUM_PROTOCOL_VERSION = "1.9.0.0";
 
-    public static readonly string VERSION = "2.2.1.0";
+    public static readonly string VERSION = "2.3.0.0";
 
     private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
--- Scripts/DCS-SRS/Scripts/DCS-SimpleRadioStandalone.lua
+++ Scripts/DCS-SRS/Scripts/DCS-SimpleRadioStandalone.lua
@@ -1,4 +1,4 @@
--- Version 2.2.1.0
+-- Version 2.3.0.0
 
 -- Special thanks to Cap. Zeen, Tarres and Splash for all the help
 -- with getting the radio information :)
@@ -7981,4 +7981,4 @@ end
 -- Load mods' SRS plugins
 SR.LoadModsPlugins()
 
-SR.log("Loaded SimpleRadio Standalone Export version: 2.2.1.0")
+SR.log("Loaded SimpleRadio Standalone Export version: 2.3.0.0")
