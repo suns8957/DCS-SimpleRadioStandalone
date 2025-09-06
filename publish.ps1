f3c7b8cf011a9eb4b6f3a60acd8f3f14ee5dd6b1
gcask <53709079+gcask@users.noreply.github.com>
2025-09-05 23:14:32 -0400

    Fix framework target version

--- publish.ps1
+++ publish.ps1
@@ -53,7 +53,7 @@ Write-Host "Publishing DCS-SR-Client..." -ForegroundColor Green
 Remove-Item "$outputPath\Client" -Recurse -ErrorAction SilentlyContinue
 dotnet clean "./DCS-SR-Client/DCS-SR-Client.csproj"
 dotnet publish "./DCS-SR-Client/DCS-SR-Client.csproj" `
-    --framework "net9.0-windows10.0.22000.0" `
+    --framework "net9.0-windows10.0.22000" `
     --runtime win-x64 `
     --output "$outputPath\Client" `
     --self-contained false `
